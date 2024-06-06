1c2da93c555f7125e4a6850ff9f09443d9787efd
Jenniferwonder <Jenniferwonder@163.com>
2024-06-06 19:05:49 +0800

    feat: add github link on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,32 +30,37 @@ export default function Navbar() {
 
   return (
     <div className="w-[100vw] fixed z-[300] px-5 pt-1 shadow-sm h-14 bg-main-bg dark:bg-secondary-dark-bg">
-      {activeMenu
-        ? (
-          <div className="flex items-center w-full">
-            <Link
-              to="/learn-react/"
-              className="flex items-center gap-3 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-            >
-              <i className="text-3xl i-mdi-library" />
-              <span>Learn React</span>
-            </Link>
-            <ToggleSidebar />
-          </div>
-          )
-        : (
-          <div className="flex items-center">
-            <Link
-              to="/learn-react/"
-              className="flex items-center gap-3 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-            >
-              <i className="text-3xl i-mdi-library" />
-              <span>Learn React</span>
-            </Link>
-            <ToggleSidebar />
-          </div>
-          )}
-
+      <div className="flex items-center justify-between pr-4">
+        {activeMenu
+          ? (
+            <div className="flex">
+              <Link
+                to="/learn-react/"
+                className="flex items-center gap-3 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+              >
+                <i className="text-3xl i-mdi-library" />
+                <span>Learn React</span>
+              </Link>
+              <ToggleSidebar />
+            </div>
+            )
+          : (
+            <div className="flex items-center">
+              <Link
+                to="/learn-react/"
+                className="flex items-center gap-3 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+              >
+                <i className="text-3xl i-mdi-library" />
+                <span>Learn React</span>
+              </Link>
+              <ToggleSidebar />
+            </div>
+            )}
+        {/* ref: https://stackoverflow.com/questions/25036935/get-anchor-tag-to-open-in-new-window */}
+        <a target="_blank" rel="noreferrer" href="https://github.com/Jenniferwonder/learn-react" className="block p-2 rounded-full bg-white-light/40 dark:bg-dark/40 hover:bg-white-light/90 dark:hover:bg-dark/60">
+          <i className="text-2xl i-carbon-logo-github" />
+        </a>
+      </div>
     </div>
   )
 }
